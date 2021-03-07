c860e01c17274d717569a97891428a0ae7b8e19b
hrlnsnchz <hrlnsnchz@gmail.com>
2021-03-06 21:03:50 -0600

    Added more data for products and productTypes in the database and rendered it to the UI.

--- src/components/products/Product.js
+++ src/components/products/Product.js
@@ -5,6 +5,8 @@ export const ProductCard = ({product}) => (
         <h3 className="product__name">
           { product.name }
         </h3>
+        <div className="id">ID: {product.id}</div>
         <div className="price">${product.price}.00</div>
+        <div className="categoryId">Category ID: {product.productTypeId}</div>
     </section>
 )
